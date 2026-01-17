aabfdc5719bced80b5f284e09a19037a7eb2c0c9
Kunal Dua <kunaldua2000@gmail.com>
2026-01-17 19:54:48 +0530

    zod validation user

---
+++ backend/schemas/userSchema.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+const userSignUp = z.object({
+    email: z.email(),
+    firstName: z.string(),
+    lastName: z.string().optional(),
+    password: z.string().min(10),
+});
+
+const userSignIn = z.object({
+    email: z.email(),
+    password: z.string().min(6),
+});
+
+const userUpdate = z.object({
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    password: z.string().min(6).optional(),
+});
+
+export { userSignUp, userSignIn, userUpdate };
