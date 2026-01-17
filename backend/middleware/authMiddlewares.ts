8a90938548dbe635e91af535300d844d3b71aaf8
Kunal Dua <kunaldua2000@gmail.com>
2026-01-17 21:34:18 +0530

    added suth middleware

---
+++ backend/middleware/authMiddlewares.ts
@@ -0,0 +1,34 @@
+import type { Request, Response, NextFunction } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    const authHeader: string | undefined = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(403).json({
+            msg: "Not Authorizated to access",
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(
+            authHeader,
+            process.env.JWT_SECRET!,
+        ) as JwtPayload;
+
+        if (typeof decoded !== "object" || !decoded.id) {
+            return res.status(403).json({
+                msg: "Not Authorizated to access",
+            });
+        }
+
+        req.userid = decoded.id as string;
+        next();
+    } catch (err) {
+        return res.status(403).json({});
+    }
+};
