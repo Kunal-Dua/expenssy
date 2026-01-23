1901680a51726a32cea7bce3aa185aca85ed8ba3
Kunal Dua <kunaldua2000@gmail.com>
2026-01-24 04:48:55 +0530

    added cors and env config

--- backend/index.ts
+++ backend/index.ts
@@ -2,11 +2,19 @@ import express from "express";
 import cors from "cors";
 import { rootRouter } from "./routes/index.js";
 import globalErrorHandler from "./middleware/error.js";
+import dotenv from "dotenv";
 const port = process.env.PORT;
+
+dotenv.config();
 
 const app = express();
 
-// app.use(cors);
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use("/api/v1", rootRouter);
 app.use(globalErrorHandler);
