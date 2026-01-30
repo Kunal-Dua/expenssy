316154badfb8cc8acb7fab23827afec157d0ee5d
Kunal Dua <kunaldua2000@gmail.com>
2026-01-31 03:52:57 +0530

    removed expenseList to tracker directly

--- frontend/src/pages/Tracker.tsx
+++ frontend/src/pages/Tracker.tsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 import AddCategory from "../components/AddCategory";
 import CategoryChart from "../components/CategoryChart";
 import CreateExpense from "../components/CreateExpense";
-import ExpenseList from "../components/ExpenseList";
 import Navbar from "../components/Navbar";
+import { useRecoilValue } from "recoil";
+import { expenseState } from "../store/atoms/expenseAtom";
+import ExpenseTable from "../components/ExpenseTable";
 
 const Tracker = () => {
+    const expense = useRecoilValue(expenseState);
     return (
         <>
             <Navbar />
             <div className="flex p-2 my-5 gap-5">
                 <div className="flex flex-col flex-1">
                     <CreateExpense />
-                    <ExpenseList />
+                    <ExpenseTable expenses={expense.slice(0, 10)} />
                     <Link to="/expenses">
                         <div>See more</div>
                     </Link>
