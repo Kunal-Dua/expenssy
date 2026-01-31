607e828c8382ead43be503bb781cbbd4a8def7bb
Kunal Dua <kunaldua2000@gmail.com>
2026-02-01 00:50:55 +0530

    signin sign up aligment fixed

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -33,56 +33,54 @@ const Signin = () => {
     }
 
     return (
-        <div>
-            <div className="grid grid-cols-2">
-                <div>
-                    <div className="h-screen flex justify-center flex-col">
-                        <div className="flex justify-center">
-                            <div>
-                                <div className="text-3xl font-extrabold">
-                                    Create account
-                                </div>
-                                Don't have an account?
-                                <Link className="pl-2 underline" to={"/signin"}>
-                                    Sign up
-                                </Link>
+        <div className="grid grid-cols-2">
+            <div>
+                <div className="h-screen flex items-center justify-center">
+                    <div className="flex flex-col w-full max-w-md">
+                        <div>
+                            <div className="text-3xl font-extrabold">
+                                Create account
                             </div>
-                            <div className="pt-8">
-                                <LabelledInput
-                                    label="Email"
-                                    placeholder="Enter Email"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            email: e.target.value,
-                                        });
-                                    }}
-                                />
-                                <LabelledInput
-                                    label="Password"
-                                    placeholder="Enter Password"
-                                    type="password"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            password: e.target.value,
-                                        });
-                                    }}
-                                />
-                                <button
-                                    onClick={sendRequest}
-                                    type="button"
-                                    className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                                >
-                                    Sign in
-                                </button>
-                            </div>
+                            Don't have an account?
+                            <Link className="pl-2 underline" to={"/signin"}>
+                                Sign up
+                            </Link>
+                        </div>
+                        <div className="pt-8">
+                            <LabelledInput
+                                label="Email"
+                                placeholder="Enter Email"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        email: e.target.value,
+                                    });
+                                }}
+                            />
+                            <LabelledInput
+                                label="Password"
+                                placeholder="Enter Password"
+                                type="password"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        password: e.target.value,
+                                    });
+                                }}
+                            />
+                            <button
+                                onClick={sendRequest}
+                                type="button"
+                                className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                            >
+                                Sign in
+                            </button>
                         </div>
                     </div>
                 </div>
-                <div className="hidden lg:block">
-                    <Quote />
-                </div>
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
             </div>
         </div>
     );
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -33,78 +33,76 @@ const Signup = () => {
         }
     }
     return (
-        <div>
-            <div className="grid grid-cols-2">
-                <div>
-                    <div className="h-screen flex justify-center flex-col">
-                        <div className="flex justify-center">
-                            <div>
-                                <div className="text-3xl font-extrabold">
-                                    Create account
-                                </div>
-                                Already have an account?
-                                <Link className="pl-2 underline" to={"/signin"}>
-                                    Sign In
-                                </Link>
+        <div className="grid grid-cols-2">
+            <div>
+                <div className="h-screen flex items-center justify-center">
+                    <div className="flex flex-col w-full max-w-md">
+                        <div>
+                            <div className="text-3xl font-extrabold">
+                                Create account
                             </div>
-                            <div className="pt-8">
-                                <LabelledInput
-                                    label="Email"
-                                    placeholder="Enter Email"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            email: e.target.value,
-                                        });
-                                    }}
-                                />
+                            Already have an account?
+                            <Link className="pl-2 underline" to={"/signin"}>
+                                Sign In
+                            </Link>
+                        </div>
+                        <div className="pt-8">
+                            <LabelledInput
+                                label="Email"
+                                placeholder="Enter Email"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        email: e.target.value,
+                                    });
+                                }}
+                            />
 
-                                <LabelledInput
-                                    label="First Name"
-                                    placeholder="Enter First Name"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            firstName: e.target.value,
-                                        });
-                                    }}
-                                />
-                                <LabelledInput
-                                    label="Last Name"
-                                    placeholder="Enter Last Name"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            lastName: e.target.value,
-                                        });
-                                    }}
-                                />
-                                <LabelledInput
-                                    label="Password"
-                                    placeholder="Enter Password"
-                                    type="password"
-                                    onChange={(e) => {
-                                        setInputs({
-                                            ...inputs,
-                                            password: e.target.value,
-                                        });
-                                    }}
-                                />
-                                <button
-                                    onClick={sendRequest}
-                                    type="button"
-                                    className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                                >
-                                    Sign Up
-                                </button>
-                            </div>
+                            <LabelledInput
+                                label="First Name"
+                                placeholder="Enter First Name"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        firstName: e.target.value,
+                                    });
+                                }}
+                            />
+                            <LabelledInput
+                                label="Last Name"
+                                placeholder="Enter Last Name"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        lastName: e.target.value,
+                                    });
+                                }}
+                            />
+                            <LabelledInput
+                                label="Password"
+                                placeholder="Enter Password"
+                                type="password"
+                                onChange={(e) => {
+                                    setInputs({
+                                        ...inputs,
+                                        password: e.target.value,
+                                    });
+                                }}
+                            />
+                            <button
+                                onClick={sendRequest}
+                                type="button"
+                                className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                            >
+                                Sign Up
+                            </button>
                         </div>
                     </div>
-                    );
                 </div>
-                <div className="hidden lg:block">
-                    <Quote />
-                </div>
+                );
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
             </div>
         </div>
     );
