2df6bf7e97460c0ed7b6c2e9442635c4e0a39bc9
Kunal Dua <kunaldua2000@gmail.com>
2026-02-01 03:03:04 +0530

    cors added

--- backend/index.ts
+++ backend/index.ts
@@ -14,6 +14,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "https://expenssy-frontend.vercel.app",
+            "https://expenssy-frontend-511gge0tx-kunal-duas-projects.vercel.app",
         ],
         credentials: true,
     }),
