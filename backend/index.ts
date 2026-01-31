6e69eac53f58edc0cda37501d75c3e9cec71ad18
Kunal Dua <kunaldua2000@gmail.com>
2026-02-01 03:11:05 +0530

    new added

--- backend/index.ts
+++ backend/index.ts
@@ -15,7 +15,16 @@ const allowedOrigins = [
 ];
 app.use(
     cors({
-        origin: allowedOrigins,
+        origin: (origin, callback) => {
+            // allow server-to-server & Postman
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
